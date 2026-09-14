a6e6c91d9f0fb4673d35a45a08ab48b47daa5293
Softdown-prog <rutsdrigo@gmail.com>
2026-09-14 17:25:07 -0300

    Gate authoring while canonical viewport is inspection-only

--- C++/MapForge2/src/main_window.h
+++ C++/MapForge2/src/main_window.h
@@ -23,10 +23,15 @@ private:
     void buildDocks();
     void refreshHistoryActions();
     void setTool(EditorTool tool);
+    void setAuthoringEnabled(bool enabled);
 
     EditorCanvas* canvas_ = nullptr;
     QAction* undo_action_ = nullptr;
     QAction* redo_action_ = nullptr;
+    QAction* inspect_action_ = nullptr;
+    QAction* terrain_action_ = nullptr;
+    QAction* road_action_ = nullptr;
+    QAction* erase_action_ = nullptr;
     QLabel* tile_status_ = nullptr;
     QComboBox* terrain_combo_ = nullptr;
     QComboBox* brush_combo_ = nullptr;
