418a399692ca744546a73040fa1555b1ff34fe6a
Softdown-prog <rutsdrigo@gmail.com>
2026-09-14 20:48:52 -0300

    Add production Studio panel interface

---
+++ C++/MapForge2/src/production_studio_panel.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <QWidget>
+
+class QLabel;
+class QLineEdit;
+class QListWidget;
+class QPlainTextEdit;
+class QScrollArea;
+class QTabWidget;
+
+namespace ch::studio {
+
+class ProductionStudioPanel final : public QWidget {
+public:
+    explicit ProductionStudioPanel(QWidget* parent = nullptr);
+
+private:
+    QWidget* buildProjectPage();
+    QWidget* buildArtPage();
+    QWidget* buildMasksPage();
+    QWidget* buildAssemblyPage();
+    QWidget* buildAnimationPage();
+    QWidget* buildLightingPage();
+    QWidget* buildCameraPage();
+    QWidget* buildLibraryPage();
+    QWidget* buildValidationPage();
+    QWidget* buildExportPage();
+
+    void chooseBaseAsset();
+    void indexLibraryFolder();
+    void validateCurrentAsset();
+    void refreshAssetPreview();
+    void refreshProjectSummary();
+    bool loadProjectProfile(const QString& path, QString* error = nullptr);
+    bool saveProjectProfile(const QString& path, QString* error = nullptr) const;
+
+    QString current_asset_path_;
+    QString current_library_path_;
+
+    QTabWidget* tabs_ = nullptr;
+    QLineEdit* project_id_ = nullptr;
+    QLineEdit* project_name_ = nullptr;
+    QLabel* project_summary_ = nullptr;
+    QLabel* asset_preview_ = nullptr;
+    QLabel* asset_metadata_ = nullptr;
+    QLabel* library_summary_ = nullptr;
+    QListWidget* library_list_ = nullptr;
+    QPlainTextEdit* validation_report_ = nullptr;
+};
+
+} // namespace ch::studio
