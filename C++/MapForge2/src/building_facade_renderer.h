8998313667fd7af8d62f41bbe8035172bcb1405a
Softdown-prog <rutsdrigo@gmail.com>
2026-09-18 20:13:34 -0300

    Add modular facade renderer interface

---
+++ C++/MapForge2/src/building_facade_renderer.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "building_composer.h"
+
+#include <QImage>
+#include <QJsonObject>
+#include <QSize>
+
+namespace ch::studio {
+
+class BuildingFacadeRenderer final {
+public:
+    static QImage renderView(const BuildingComposerSpec& spec, BuildingView view,
+                             QSize canvas = QSize(320, 280));
+    static QImage renderSpriteSheet(const BuildingComposerSpec& spec,
+                                    QSize cell = QSize(320, 280));
+    static QImage renderReviewSheet(const BuildingComposerSpec& spec,
+                                    QSize cell = QSize(300, 260));
+    static QJsonObject manifest(const BuildingComposerSpec& spec);
+};
+
+} // namespace ch::studio
