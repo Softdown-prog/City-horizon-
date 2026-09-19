d6cf62196b5038af6a66d46475d3286008935093
Softdown-prog <rutsdrigo@gmail.com>
2026-09-18 23:48:37 -0300

    Add canonical carousel part library interface

---
+++ C++/MapForge2/src/carousel_part_library.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "animation_core.h"
+
+#include <QImage>
+#include <QJsonObject>
+#include <QPointF>
+#include <QSizeF>
+#include <QString>
+#include <QStringList>
+
+namespace ch::studio {
+
+class CarouselPartLibrary final {
+public:
+    static constexpr const char* kVersion = "carousel_parts_1";
+
+    static QStringList partIds();
+    static bool contains(const QString& part_id);
+    static QString displayName(const QString& part_id);
+    static QSizeF defaultSize(const QString& part_id);
+    static QPointF defaultPivot(const QString& part_id);
+
+    static AnimationNodeSpec makeNode(const QString& part_id,
+                                      const QString& node_id,
+                                      const QString& parent_id,
+                                      const QPointF& position_px,
+                                      int draw_order,
+                                      const QColor& primary = QColor("#c9554f"),
+                                      const QColor& outline = QColor("#343638"));
+
+    static QImage renderPart(const QString& part_id,
+                             const QSize& target_size,
+                             const QColor& primary,
+                             const QColor& outline,
+                             QString* reason = nullptr);
+
+    static QJsonObject manifest();
+};
+
+} // namespace ch::studio
