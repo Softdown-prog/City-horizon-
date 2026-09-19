640a42687bf09221d658d9accdb4b1e297b7ef1a
Softdown-prog <rutsdrigo@gmail.com>
2026-09-18 23:22:25 -0300

    Add animation export pipeline interface

---
+++ C++/MapForge2/src/animation_export_pipeline.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "animation_core.h"
+
+#include <QString>
+#include <QStringList>
+
+namespace ch::studio {
+
+struct AnimationExportResult {
+    bool success = false;
+    QString reason;
+    QString output_directory;
+    QStringList files;
+};
+
+class AnimationExportPipeline final {
+public:
+    static constexpr const char* kVersion = "animation_export_1";
+
+    static AnimationExportResult exportClip(const AnimatedAssetSpec& asset,
+                                            const QString& clip_id,
+                                            const QString& output_directory);
+};
+
+} // namespace ch::studio
