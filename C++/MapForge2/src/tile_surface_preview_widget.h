4bcb92afd654d50c2ef8db1098a4c19c07071be6
Softdown-prog <rutsdrigo@gmail.com>
2026-09-20 13:19:19 -0300

    studio: add tile surface preview widget

---
+++ C++/MapForge2/src/tile_surface_preview_widget.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <QPixmap>
+#include <QWidget>
+
+namespace ch::studio {
+
+class TileSurfacePreviewWidget final : public QWidget {
+public:
+    explicit TileSurfacePreviewWidget(QWidget* parent = nullptr);
+
+    void setTile(const QPixmap& tile);
+    void clearTile();
+    void setRepeatCount(int repeat_x, int repeat_y);
+    void setShowGrid(bool show_grid);
+    void setAutotileMask(int mask);
+
+protected:
+    void paintEvent(QPaintEvent* event) override;
+
+private:
+    [[nodiscard]] QPointF projectTile(qreal tile_x, qreal tile_y) const;
+
+    QPixmap tile_;
+    int repeat_x_ = 5;
+    int repeat_y_ = 5;
+    bool show_grid_ = true;
+    int autotile_mask_ = 15;
+};
+
+} // namespace ch::studio
