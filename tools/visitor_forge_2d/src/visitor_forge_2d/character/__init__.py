8f9aecf24427fa5b3f109a8835177f00e161d954
Softdown-prog <rutsdrigo@gmail.com>
2026-09-24 01:14:11 -0300

    visitor forge 2d: expose procedural art generator

--- tools/visitor_forge_2d/src/visitor_forge_2d/character/__init__.py
+++ tools/visitor_forge_2d/src/visitor_forge_2d/character/__init__.py
@@ -1,3 +1,9 @@
+from .procedural_art import STYLE_CONTRACT, generate_v1_south_assets
 from .schema import REQUIRED_V1_PARTS, validate_v1_character
 
-__all__ = ["REQUIRED_V1_PARTS", "validate_v1_character"]
+__all__ = [
+    "REQUIRED_V1_PARTS",
+    "STYLE_CONTRACT",
+    "generate_v1_south_assets",
+    "validate_v1_character",
+]
